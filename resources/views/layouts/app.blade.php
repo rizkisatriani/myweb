7055dda22f6ee9d16fe57b70bdfe192df01ca5d6
rizkisatriani <agungrizki0@gmail.com>
2025-03-02 11:49:50 +0700

    fix conflict

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,9 +38,16 @@
     <meta name="language" content="{{ app()->getLocale()}}">
     <meta name="robots" content="index, follow">
     <script async defer src="https://buttons.github.io/buttons.js"></script>
-    <meta name="google-site-verification" content="-dM_qQnrtpcnhZSfeDBgaavV4RQt8DkPuVJV0BvEHWs" />
+    <meta name="google-site-verification" content="-dM_qQnrtpcnhZSfeDBgaavV4RQt8DkPuVJV0BvEHWs" /> 
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9168454602694889"
-     crossorigin="anonymous"></script>
+     crossorigin="anonymous"></script> 
+    <!-- Event snippet for Website sale conversion page -->
+    <script>
+      gtag('event', 'conversion', {
+          'send_to': 'AW-872504487/vHuNCLne-8cDEKe5haAD',
+          'transaction_id': ''
+      });
+    </script> 
 </head>
 <body>
     @include('components.topnav') <!-- Navigasi -->
