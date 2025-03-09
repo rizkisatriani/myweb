8016836877a29f9285cc3b253acce0f935499a4b
Rizki Agung Prasetyo <rizki@xdemia.com>
2025-03-09 14:12:45 +0700

    fix gray bg

--- resources/views/convertdoc.blade.php
+++ resources/views/convertdoc.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <section class="bg-white dark:bg-gray-900">
-    <div class="bg-gray-200 max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+    <div class="max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
         <div class="container mx-auto">
             <!-- Breadcrumb -->
             <div class="text-sm text-gray-600 mb-4">
