41a9c8600a9038db9b3d221e6853b9e43b730494
Rizki Agung Prasetyo <rizki@xdemia.com>
2025-03-14 13:41:23 +0700

    fix meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,9 @@
     <meta name="theme-color" content="#ffffff">
     <link rel="stylesheet" href="<?= asset('css/output.css'); ?>">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <meta name="language" content="{{ app()->getLocale() == 'id' ? 'Indonesia' : 'English' }}">
+    <meta name="robots" content="index, follow">
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <meta name="google-site-verification" content="-dM_qQnrtpcnhZSfeDBgaavV4RQt8DkPuVJV0BvEHWs" />
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9168454602694889"
