f40d0660edb6fb469a6b58a19584a6a13d7d7578
Rizki Agung Prasetyo <rizki@xdemia.com>
2025-07-27 13:36:23 +0700

    fix sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,7 +1,6 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
->
+@php echo '<?xml version="1.0" encoding="UTF-8"?>'; @endphp
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
     <url>
         <loc>{{ url('/') }}</loc>
         <lastmod>{{ now()->toAtomString() }}</lastmod>
@@ -17,4 +16,5 @@
             <priority>0.8</priority>
         </url>
     @endforeach
+
 </urlset>
