a5e96c068d8d554d90883da914a42416e1048d40
Rizki Agung Prasetyo <rizki@xdemia.com>
2025-07-27 13:18:05 +0700

    add blog menu

--- resources/views/components/topnav.blade.php
+++ resources/views/components/topnav.blade.php
@@ -86,7 +86,7 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="/en/speed-test-internet" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Speed Test Internet</a>
+                        <a href="/blogs" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Blogs</a>
                     </li>
                 </ul>
             </div>
